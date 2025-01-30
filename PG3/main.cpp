273590b69ca4681fdfd07b97d5a97580edeedd5d
mutoairi <146906672+mutoairi@users.noreply.github.com>
2025-01-31 08:12:40 +0900

    kaan

--- PG3/main.cpp
+++ PG3/main.cpp
@@ -7,21 +7,21 @@
 int main() {
 
 	printf("100000文字を移動とコピーで比較しました。\n");
-	std::string a(100000, 'a');
+	std::string a(1000000, 'a');
 
 	std::chrono::system_clock::time_point start,end;
 
 	start = std::chrono::system_clock::now();
 	std::string b = a;
 	end = std::chrono::system_clock::now();
-	std::chrono::duration<double,std::micro> copyTime= (end - start);
+	std::chrono::duration<long double,std::micro> copyTime= (end - start);
 	std::cout << "コピー: " << copyTime.count() << " us"<<std::endl;
 
 	//移動
 	start = std::chrono::system_clock::now();
-	std::string c = std::move(a);
+	std::string&& c = std::move(a);
 	end = std::chrono::system_clock::now();
-	std::chrono::duration<double, std::micro> moveTime = end - start;
+	std::chrono::duration<long double, std::micro> moveTime = end - start;
 	std::cout << "移動: " << moveTime.count() << " us" << std::endl;
 
 	return 0;
